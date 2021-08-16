8a3ee4299ada88c96a48715431b10e0c5ab37196
Justin Marrington <justin@Justins-MacBook-Pro.local>
2021-08-16 04:53:16 +0000

    Implemented number frequency generator object

---
+++ src/frequencyGenerator/frequencyGenerator.test.ts
@@ -0,0 +1,35 @@
+import range from "lodash/range"
+
+import { FrequencyGenerator } from "./"
+
+let frequencyGenerator: FrequencyGenerator
+
+const appendXTimes = (generator: FrequencyGenerator, inputNumber: number, frequency: number) => {
+	range(frequency).forEach(() => {
+		generator.append(inputNumber)
+	})
+}
+
+beforeEach(() => {
+	frequencyGenerator = new FrequencyGenerator()
+})
+
+test("generator initialises with an empty reporting set", () => {
+  expect(frequencyGenerator.report()).toBe("")
+})
+
+test("generator accepts number input", () => {
+	frequencyGenerator.append(10)
+	expect(frequencyGenerator.report()).toBe("10:1")
+	
+	frequencyGenerator.append(10)
+	expect(frequencyGenerator.report()).toBe("10:2")
+})
+
+test("generator reports numbers in descending order of input frequency", () => {
+	appendXTimes(frequencyGenerator, 10, 2)
+	appendXTimes(frequencyGenerator, 3, 5)
+	appendXTimes(frequencyGenerator, 15, 1)
+	
+	expect(frequencyGenerator.report()).toBe("3:5, 10:2, 15:1")
+})
