5248f91c904487adb6a63338a4516cd8facc9f9a
Justin Marrington <justin@marrington.net>
2021-08-17 08:14:50 +0000

    Extemded frequency reporter to report tuples

--- src/frequencyGenerator/frequencyGenerator.ts
+++ src/frequencyGenerator/frequencyGenerator.ts
@@ -1,26 +1,32 @@
 type Frequencies = Map<number, number>
+type ReportingTuples = [number, number][]
 
 export class FrequencyGenerator {
-	frequencies: Frequencies
+  frequencies: Frequencies
 
-	constructor() {
-		this.frequencies = new Map<number, number>()
-	}
+  constructor() {
+    this.frequencies = new Map<number, number>()
+  }
 
-	append(number: number) {
-		const existingFrequency = this.frequencies.get(number) || 0
-		this.frequencies.set(number, existingFrequency + 1)
-	}
-	
-	report(): string {
-		const inputNumbers = Array.from(this.frequencies.entries())
-		return inputNumbers
-			.sort(([, aFrequency], [, bFrequency]) => {
-				return bFrequency - aFrequency
-			})
-			.map(([inputNumber,]: [number, number]) => {
-				return `${inputNumber}:${this.frequencies.get(inputNumber)}`
-			})
-			.join(", ")
-	}
+  append(number: number) {
+    const existingFrequency = this.frequencies.get(number) || 0
+    this.frequencies.set(number, existingFrequency + 1)
+  }
+
+  report(): ReportingTuples {
+    const inputNumbers = Array.from(this.frequencies.entries())
+    return inputNumbers
+      .sort(([, aFrequency], [, bFrequency]) => {
+        return bFrequency - aFrequency
+      })
+      .map(([inputNumber]: [number, number]) => {
+        return [inputNumber, this.frequencies.get(inputNumber) || 0]
+      })
+  }
+
+  toString(): string {
+    return this.report()
+      .map(([inputNumber, frequency]) => `${inputNumber}:${frequency}`)
+      .join(", ")
+  }
 }
