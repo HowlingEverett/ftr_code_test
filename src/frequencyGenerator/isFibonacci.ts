940ff6a6fc4f3338503b895bd0059dd1a4f1b831
Justin Marrington <justin@marrington.net>
2021-08-23 01:45:26 +1000

    Add naive BigInt protection to fibonacci checker

--- src/frequencyGenerator/isFibonacci.ts
+++ src/frequencyGenerator/isFibonacci.ts
@@ -1,4 +1,5 @@
 export const isFibonacci = (n: number) => {
+
   // Using the simple implementation of Binet's formula here
   // For the 'letter' of the spec, a simpler implementation might simple be
   // [0, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987].includes(n)
@@ -6,12 +7,15 @@ export const isFibonacci = (n: number) => {
 }
 
 const perfectSquare = (n: number): boolean => {
+
+  // Naive defence against really large numbers, a better solution would use
+  // BigInt but the spec here only cares about numbers < 1000.
+  if (n >= Number.MAX_SAFE_INTEGER) {
+    return false
+  }
   const squareInt = Math.floor(Math.sqrt(n))
 
   // We're a perfect square if Math.floor on the square root
   // has performed a no-op, essentially.
-  //
-  // FIXME: This implementation will fail horribly on numbers larger than a
-  // 64-bit integer, but the specification limits us to fibs below 1000.
   return squareInt * squareInt === n
 }
