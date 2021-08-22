bdd7f2bba9aa7cdd1fcc6b42d39cf30033ec3aab
Justin Marrington <justin@marrington.net>
2021-08-23 01:50:48 +1000

    Implemented top level behaviour - time based counting/reporting

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,106 @@
-import { render, screen } from "@testing-library/react"
+import { fireEvent, render, screen, act } from "@testing-library/react"
 import App from "./App"
 
-test("renders learn react link", () => {
-  render(<App />)
-  const linkElement = screen.getByText(/learn react/i)
-  expect(linkElement).toBeInTheDocument()
+describe("Frequency Counter web application", () => {
+  beforeEach(() => {
+    jest.useFakeTimers()
+  })
+
+  afterEach(() => {
+    act(() => {
+      jest.runOnlyPendingTimers()
+    })
+    jest.useRealTimers()
+  })
+
+  test("renders the UI initially with frequency input active", () => {
+    render(<App />)
+
+    const frequencyInput = screen.getByLabelText("Enter a reporting frequency")
+    expect(frequencyInput).toBeInTheDocument()
+    expect(frequencyInput).not.toHaveAttribute("disabled")
+  })
+
+  test("renders the UI initially with the number input disabled", () => {
+    render(<App />)
+
+    const numberInput = screen.getByLabelText("Enter a number to count")
+    expect(numberInput).toBeInTheDocument()
+    expect(numberInput).toHaveAttribute("disabled")
+  })
+
+  test("renders the test logger with a no inputs message", () => {
+    render(<App />)
+
+    const frequencyLogger = screen.getByTestId("frequency-logger")
+    expect(frequencyLogger).toBeInTheDocument()
+    expect(frequencyLogger).toHaveTextContent("No numbers recorded")
+  })
+
+  test("accepts a valid frequency value and starts the application", () => {
+    render(<App />)
+
+    const frequencyInput = screen.getByLabelText("Enter a reporting frequency")
+    const startButton = screen.getByText("Start")
+
+    fireEvent.change(frequencyInput, { target: { value: "10" } })
+    fireEvent.click(startButton)
+
+    const frequencyReportLabel = screen.getByText(/Reporting frequency:/)
+    expect(frequencyReportLabel).toBeInTheDocument()
+    expect(frequencyReportLabel).toHaveTextContent(
+      "Reporting frequency: 10 seconds"
+    )
+
+    expect(frequencyInput).toHaveAttribute("disabled")
+    expect(startButton).toHaveTextContent("Pause")
+    const numberInput = screen.getByLabelText("Enter a number to count")
+    expect(numberInput).not.toHaveAttribute("disabled")
+  })
+
+  test("updates input counts while timer is running", async () => {
+    render(<App />)
+
+    const frequencyInput = screen.getByLabelText("Enter a reporting frequency")
+    const startButton = screen.getByText("Start")
+    const numberInput = screen.getByLabelText("Enter a number to count")
+    const countButton = screen.getByText("Count")
+    const reportLog = screen.getByTestId("frequency-logger")
+
+    // Set a reporting frequency of 10 seconds, and kick off the reporter
+    fireEvent.change(frequencyInput, { target: { value: "10" } })
+    fireEvent.click(startButton)
+
+    // Count some numbers
+    fireEvent.change(numberInput, { target: { value: "12" } })
+    fireEvent.click(countButton)
+    fireEvent.change(numberInput, { target: { value: "6" } })
+    fireEvent.click(countButton)
+    fireEvent.change(numberInput, { target: { value: "6" } })
+    fireEvent.click(countButton)
+
+    act(() => {
+      jest.advanceTimersByTime(10001)
+    })
+
+    expect(reportedText(reportLog)).toEqual(["6:2", "12:1"])
+
+    // Count again
+    fireEvent.change(numberInput, { target: { value: "6" } })
+    fireEvent.click(countButton)
+
+    expect(reportedText(reportLog)).toEqual(["6:2", "12:1"])
+
+    act(() => {
+      jest.advanceTimersByTime(10001)
+    })
+
+    expect(reportedText(reportLog)).toEqual(["6:3", "12:1"])
+  })
 })
+
+const reportedText = (reportLog: HTMLElement): string[] => {
+  return Array.from(reportLog.getElementsByTagName("li")).map((listElement) => {
+    return listElement.textContent || ""
+  })
+}
--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,72 @@
-import logo from "./logo.svg"
+import { useReducer, useState } from "react"
+import useInterval from "react-useinterval"
+
 import "./App.css"
 
+import { FrequencyInput } from "./components/FrequencyInput"
+import { NumberInput } from "./components/NumberInput"
+import { FrequencyLogger } from "./components/FrequencyLogger"
+import {
+  frequencyReducer,
+  FrequencyReducerState,
+  reportingTuples,
+  SET_REPORTING_FREQUENCY,
+  SET_RUNNING,
+  COUNT_NUMBER,
+} from "./frequencyGenerator/frequencyReducer"
+
+const INITIAL_STATE: FrequencyReducerState = {
+  numberFrequencies: {},
+  isRunning: false,
+}
+
 function App() {
+  const [state, dispatch] = useReducer(frequencyReducer, INITIAL_STATE)
+  const [countBuffer, setCountBuffer] = useState<number[]>([])
+  const { isRunning, reportingFrequency = 0 } = state
+
+  const flushCount = () => {
+    // Flush the count buffer into the store on reportingFrequency interval
+    countBuffer.forEach((n) => {
+      dispatch({ type: COUNT_NUMBER, numberToCount: n })
+    })
+    setCountBuffer([])
+  }
+
+  useInterval(flushCount, isRunning ? reportingFrequency * 1000 : null)
+
+  const onFrequencyInput = (frequency: number) => {
+    if (isRunning) {
+      dispatch({ type: SET_RUNNING, isRunning: false })
+    } else {
+      dispatch({ type: SET_REPORTING_FREQUENCY, frequency })
+      dispatch({ type: SET_RUNNING, isRunning: true })
+    }
+  }
+  const onNumberInput = (n: number) => {
+    // In order to simulate 'only update the UI on a set frequncy, we buffer
+    // input counting instead of writing them directly to the store
+    setCountBuffer([...countBuffer, n])
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App-content">
+        <FrequencyInput
+          onFrequencyInput={onFrequencyInput}
+          isRunning={isRunning}
+        />
+
+        <NumberInput onNumberInput={onNumberInput} isRunning={isRunning} />
+
+        {isRunning && (
+          <div className="reporting-frequency">
+            Reporting frequency: {reportingFrequency} seconds
+          </div>
+        )}
+
+        <FrequencyLogger report={reportingTuples(state)} />
+      </div>
     </div>
   )
 }
