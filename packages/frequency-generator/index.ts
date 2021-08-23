e04e4bd03cb123adc8441efcc1647689be43d944
Justin Marrington <justin@marrington.net>
2021-08-24 09:04:34 +1000

    Run tests in each package

--- packages/frequency-generator/index.ts
+++ packages/frequency-generator/index.ts
@@ -1,7 +1,3 @@
-export {
-  frequencyReducer,
-  reportingString,
-  reportingTuples,
-} from "./frequencyReducer"
+export * from "./frequencyReducer"
 export type { ReportingTuples } from "./frequencyReducer"
 export { isFibonacci } from "./isFibonacci"
